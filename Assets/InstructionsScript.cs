ef190f6f9a3c0711f6b6ceb2a8b7eab3031c3c70
Chloe <cy214@sussex.ac.uk>
2022-04-19 22:35:06 +0100

    changed text

--- Assets/InstructionsScript.cs
+++ Assets/InstructionsScript.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class InstructionsScript : MonoBehaviour
 {
     [SerializeField] private GameObject Instructions;
-
+    [SerializeField] private GameObject TextFillGoogle; // checks if the scenario is not running yet
+    private bool IPressed = false;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.I))
+        if (Input.GetKey(KeyCode.I) && !(TextFillGoogle.activeInHierarchy))
         {
+            IPressed = true;
+            Time.timeScale = 0;
             Instructions.SetActive(true);
         }
         else
         {
-            Instructions.SetActive(false);
+            if(!TextFillGoogle.activeInHierarchy){
+                Instructions.SetActive(false);
+                if (IPressed)
+                {
+                    Time.timeScale = 1;
+                }
+
+                IPressed = false;
+            }
         }
     }
 }
